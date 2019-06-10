3bb87e5bb6a505ad363e910c2467dc9523a18eaf
Malov <malov.alexsander@yandex.ru>
2019-06-10 08:08:37 +0300

    Application context

---
+++ src/main/java/malov/medGP/PathientBean.java
@@ -0,0 +1,13 @@
+package malov.medGP;
+
+public class PathientBean {
+    private String name;
+
+    public PathientBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/malov/medGP/Test.java
@@ -0,0 +1,18 @@
+package malov.medGP;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        PathientBean pathientBean = context.getBean("pathientBean", PathientBean.class);
+
+        System.out.println(pathientBean.getName());
+
+        context.close();
+    }
+
+}
