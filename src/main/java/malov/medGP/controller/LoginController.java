92fc807091888a4e1e99357f24f2c21a93f62709
Malov <malov.alexsander@yandex.ru>
2019-06-13 01:57:12 +0300

    MVC

---
+++ src/main/java/malov/medGP/controller/LoginController.java
@@ -0,0 +1,18 @@
+package malov.medGP.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allLogin() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+}
---
+++ src/main/java/malov/medGP/model/User.java
@@ -0,0 +1,6 @@
+package malov.medGP.model;
+
+public class User {
+    private int id;
+    private String name;
+}
